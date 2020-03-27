3d46738f70321801a9b0ed37c229e8b4c869f663
Chiho <yyflying11d@163.com>
2020-03-27 19:56:02 +0800

    chore: 设置src文件的别名'@'

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,10 @@
-const { override, addPostcssPlugins } = require('customize-cra')
+const { override, addPostcssPlugins, addWebpackAlias } = require('customize-cra')
+const path = require('path')
 
 module.exports = override(
   addPostcssPlugins([
-    require('postcss-px-to-viewport')({ // 把px单位转换为vw、vh、vmin或者vmax这样的视窗单位
+    require('postcss-px-to-viewport')({
+      // 把px单位转换为vw、vh、vmin或者vmax这样的视窗单位
       viewportWidth: 375, // (Number) The width of the viewport.
       viewportHeight: 667, // (Number) The height of the viewport.
       unitPrecision: 3, // (Number) The decimal numbers to allow the REM units to grow to.
@@ -11,14 +13,17 @@ module.exports = override(
       minPixelValue: 1, // (Number) Set the minimum pixel value to replace.
       mediaQuery: false // (Boolean) Allow px to be converted in media queries.
     }),
-    require('postcss-write-svg')({ // 主要用来处理移动端1px问题
+    require('postcss-write-svg')({
+      // 主要用来处理移动端1px问题
       utf8: false
     }),
-    require('cssnano')({ // 主要用来压缩和清理CSS代码
+    require('cssnano')({
+      // 主要用来压缩和清理CSS代码
       'cssnano-preset-advanced': {
         zindex: false,
         autoprefixer: false
       }
     })
-  ])
+  ]),
+  addWebpackAlias({ ['@']: path.resolve(__dirname, 'src') }) // 设置src文件的别名'@'
 )
--- src/containers/Find/components/HotwallNav/index.js
+++ src/containers/Find/components/HotwallNav/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { CSSTransition, TransitionGroup } from 'react-transition-group'
-import useInterval from '../../../../hooks/useInterval'
+import useInterval from '@/hooks/useInterval'
 
 import './index.scss'
 
