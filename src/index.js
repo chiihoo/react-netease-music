cc27a6ff37ac4af05b5cc75b86773627a23d9f9e
Chiho <yyflying11d@163.com>
2020-04-10 18:52:00 +0800

    chore: icon改用线上cdn

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-import './assets/iconfont/iconfont.css'
 import { Provider } from 'react-redux'
 import store from './store'
 
